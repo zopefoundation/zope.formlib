4fc0a99a20dc03e01c5385ae3134ed2fb6739f96
Baiju Muthukadan <baiju.m.mail@gmail.com>
2007-01-18 13:32:59 +0000

    Eggify package.

--- README.txt
+++ README.txt
@@ -1,9 +1,39 @@
-Python-oriented oage and form library
-=====================================
+zope.formlib Package Readme
+===========================
 
-This project provides a set of modules for defining pages and forms
-with Python code (as opposed to ZCML).  The project provides the
-zope.formlib package.  The formlib package depends on zope.app and
-this needs pretty much a full application server to be tested and
-used.   To use the package, check out, copy, or link (via svn
-externals) the formlib package into a zope package.
+Overview
+--------
+
+Forms are web components that use widgets to display and input data.
+Typically a template displays the widgets by accessing an attribute or
+method on an underlying class.
+
+Changes
+-------
+
+See CHANGES.txt.
+
+Installation
+------------
+
+See INSTALL.txt.
+
+
+Developer Resources
+-------------------
+
+- Subversion browser:
+
+  http://svn.zope.org/zope.formlib/
+
+- Read-only Subversion checkout:
+
+  $ svn co svn://svn.zope.org/repos/main/zope.formlib/trunk
+
+- Writable Subversion checkout:
+
+  $ svn co svn+ssh://userid@svn.zope.org/repos/main/zope.formlib/trunk
+
+- Note that the 'src/zope/testbrowser package is acutally a 'svn:externals'
+  link to the corresponding package in the Zope3 trunk (or to a specific tag,
+  for released versions of the package).
---
+++ setup.py
@@ -0,0 +1,58 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.formlib package
+
+$Id$
+"""
+
+import os
+
+try:
+    from setuptools import setup, Extension
+except ImportError, e:
+    from distutils.core import setup, Extension
+
+setup(name='zope.formlib',
+      version='3.4-dev',
+      url='http://svn.zope.org/zope.formlib',
+      license='ZPL 2.1',
+      description='Zope formlib',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description="Forms are web components that use widgets"
+                       "to display and input data.  Typically a template"
+                       "displays the widgets by accessing an attribute or"
+                       "method on an underlying class.",
+
+      packages=['zope', 'zope.formlib'],
+      package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      tests_require = ['zope.testing'],
+      install_requires=['zope.interface',
+                        'zope.schema',
+                        'zope.component',
+                        'zope.publisher',
+                        'zope.event',
+                        'zope.i18n',
+                        'zope.security',
+                        'zope.app.publisher',
+                        'zope.app.traversing',
+                        'zope.i18nmessageid',
+                        'zope.app.pagetemplate',
+                        'zope.app.form',],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
