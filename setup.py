992c172ac999cde02f34ba368dd9209665b5291f
Christian Theune <ct@gocept.com>
2007-04-22 11:00:25 +0000

    Updating version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='zope.formlib',
-      version='3.4dev',
+      version = '3.4.0a1',
       url='http://svn.zope.org/zope.formlib',
       license='ZPL 2.1',
       description='Zope formlib',
