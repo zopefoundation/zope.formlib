50ec30e5fcb5e697e79f5787a594501fdc32b6ad
Michael Howitz <mh@gocept.com>
2009-05-18 20:03:57 +0000

    Preparing release 3.6.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '3.6.0dev'
+version = '3.6.0'
 
 setup(name='zope.formlib',
       version=version,
