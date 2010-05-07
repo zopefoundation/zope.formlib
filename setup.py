3d0832c77677e36b38b148a876a07ab1641312c0
Tres Seaver <tseaver@palladion.com>
2010-05-07 01:36:25 +0000

    svb

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '4.0.3'
+version = '4.0.4dev'
 
 setup(name='zope.formlib',
       version=version,
