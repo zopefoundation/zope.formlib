7eca2c2b8c6f5f889e49c5120e864e48fa7af845
Hanno Schlichting <hanno@hannosch.eu>
2011-08-20 13:27:16 +0000

    Prepare zope.formlib 4.0.6.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '4.0.6dev'
+version = '4.0.6'
 
 setup(name='zope.formlib',
       version=version,
