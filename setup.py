696417e9e72d3437196dc2cb0c48fbc9ffc43a6e
Michael Howitz <mh@gocept.com>
2012-03-15 08:20:53 +0000

    Back to development: 4.1.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '4.1.0'
+version = '4.1.1dev'
 
 setup(name='zope.formlib',
       version=version,
