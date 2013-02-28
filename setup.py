407cd92a36333819bff449e3568d92bcb360f1d4
Stephan Richter <stephan.richter@gmail.com>
2013-02-27 19:52:22 -0500

    Preparing release 4.3.0a1

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ def alltests():
     return unittest.TestSuite(suites)
 
 setup(name='zope.formlib',
-      version='4.3.0a1.dev0',
+      version='4.3.0a1',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Form generation and validation library for Zope',
