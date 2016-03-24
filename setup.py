f0bbec89b373e9c8ac4f88dd49f83184bb58cf17
Tres Seaver <tseaver@palladion.com>
2016-03-24 10:53:29 -0400

    Drop support for Python 2.6.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ def alltests():
     return unittest.TestSuite(suites)
 
 setup(name='zope.formlib',
-      version='4.3.1.dev0',
+      version='4.4.0.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Form generation and validation library for Zope',
@@ -64,7 +64,6 @@ setup(name='zope.formlib',
           'License :: OSI Approved :: Zope Public License',
           'Programming Language :: Python',
           'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.3',
