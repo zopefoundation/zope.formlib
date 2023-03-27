9264864154a744e6d01c36019520d2253d808ffe
Michael Howitz <mh@gocept.com>
2023-03-27 11:39:55 +0200

    Back to development: 6.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def read(*rnames):
 
 
 setup(name='zope.formlib',
-      version='6.0',
+      version='6.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Form generation and validation library for Zope',
