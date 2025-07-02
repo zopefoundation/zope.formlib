728cc7346ced141a0b3e5c0e290be07eaa952e8a
Michael Howitz <icemac@gmx.net>
2025-07-02 08:36:37 +0200

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def read(*rnames):
 
 
 setup(name='zope.formlib',
-      version='6.2.dev0',
+      version='7.0.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Form generation and validation library for Zope',
