e758a492857134783a577a798114fcec278d23dc
Michael Howitz <icemac@gmx.net>
2025-09-12 09:12:46 +0200

    Back to development: 7.1

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read(*rnames):
 
 
 setup(name='zope.formlib',
-      version='7.0',
+      version='7.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Form generation and validation library for Zope',
