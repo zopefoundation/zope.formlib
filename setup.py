630b8821d27a48e1bb468db4a889ee868327d254
Michael Howitz <icemac@gmx.net>
2025-09-12 08:30:53 +0200

    Switch to PEP 420 native namespace. (#50)
    * Bumped version for breaking release.
    
    * Replace ``pkg_resources`` namespace with PEP 420 native namespace.
    
    * Switch to PEP 420 native namespace.

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@
 """
 import os
 
-from setuptools import find_packages
 from setuptools import setup
 
 
@@ -30,7 +29,7 @@ def read(*rnames):
 
 
 setup(name='zope.formlib',
-      version='6.2.dev0',
+      version='7.0.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Form generation and validation library for Zope',
@@ -61,15 +60,12 @@ setup(name='zope.formlib',
           'Framework :: Zope :: 3',
       ],
       url='https://github.com/zopefoundation/zope.formlib',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['zope'],
       extras_require={
           'test': [
               'zope.configuration',
               'zope.schema >= 6.1',
               'zope.testing',
-              'zope.testrunner',
+              'zope.testrunner >= 6.4',
           ],
           'docs': [
               'Sphinx',
